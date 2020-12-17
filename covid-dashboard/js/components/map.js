4e803dada4ca50075e9d25ebfda69a1ae524bbf9
@Noch4nce <m4rgera123@mail.ru>
2020-12-18 03:01:39 +0500

    feat: fetch covid-data

--- covid-dashboard/js/components/map.js
+++ covid-dashboard/js/components/map.js
@@ -6,3 +6,10 @@ const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">Op
 const tileLayerUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
 const tiles = leaflet.tileLayer(tileLayerUrl, { attribution });
 tiles.addTo(covidMap);
+
+const covidData = async () => {
+  const response = await fetch('https://corona.lmao.ninja/v2/countries');
+  const covidApiData = await response.json();
+};
+
+covidData();
