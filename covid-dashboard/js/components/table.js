8696ce80350943c50519c7651f3616cba903d6d2
Nastya <asekerich99@gmail.com>
2020-12-18 17:49:57 +0300

    feat: add list

--- covid-dashboard/js/components/table.js
+++ covid-dashboard/js/components/table.js
@@ -1,1 +1,21 @@
-console.log('asd');
+// async function getContries() {
+//   let response = await fetch('https://api.covid19api.com/countries');
+//   let data = await response.json();
+//   let covid = data;
+//   console.log('countries rs');
+//   console.log(covid);
+// }
+// getContries();
+
+// async function getContries2() {
+//   let response = await fetch('https://api.covid19api.com/summary');
+//   let data = await response.json();
+//   let covid = data.Countries;
+//   console.log('countries topic');
+//   console.log(covid);
+// }
+// getContries2();
+
+
+
+
--- covid-dashboard/js/index.js
+++ covid-dashboard/js/index.js
@@ -1,2 +1,4 @@
 import '../styles/scss/main.scss';
 import './components/table';
+import './components/list';
+import './components/keyboard';
