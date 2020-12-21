094e4f35d17cccadc0a7ca710b076cf778db57b8
@Noch4nce <m4rgera123@mail.ru>
2020-12-22 01:58:21 +0500

    feat: data fetch, add on chart

--- covid-dashboard/js/components/chart.js
+++ covid-dashboard/js/components/chart.js
@@ -1,18 +1,36 @@
 const ctx = document.getElementById('chart_id');
+const globalCases = [];
+const dateStage = [];
 
-const createCovidChart = () => {
+const covidData = async () => {
+  const response = await fetch('https://corona-api.com/timeline');
+  const covidApiData = await response.json();
+
+  const { data } = covidApiData;
+  // console.log(covidApiData.data[0].deaths)
+  // console.log(covidApiData)
+
+  data.forEach((el) => {
+    dateStage.push(el.date);
+    globalCases.push(el.confirmed);
+  });
+};
+
+const createCovidChart = async () => {
+  await covidData();
+
   const covidChart = new Chart(ctx, {
     type: 'line',
     data: {
       datasets: [{
         label: 'Global Cases',
-        data: [123],
+        data: globalCases,
         fill: false,
         borderColor: 'red',
         backgroundColor: 'red',
         borderWidth: 1,
       }],
-      labels: ['dec'],
+      labels: dateStage,
     },
     options: {
       scales: {
