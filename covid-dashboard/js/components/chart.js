cdb63d7bad441afa30b0377db10fbf78d3c23aa2
@Noch4nce <m4rgera123@mail.ru>
2020-12-22 20:47:50 +0500

    feat: btn-active, btn-styles, eslint

--- covid-dashboard/js/components/chart.js
+++ covid-dashboard/js/components/chart.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const chartGlobal = document.getElementById('chart_global_id');
 const globalCasesButton = document.querySelector('.chart__control_global');
 
@@ -8,6 +9,8 @@ const dailyCasesButton = document.querySelector('.chart__control_daily');
 
 const chartHundred = document.getElementById('chart_hundred_id');
 const hundredCasesButton = document.querySelector('.chart__control_hundred');
+
+const chartBtnContainer = document.querySelector('.chart__control');
 
 const globalConfirmed = [];
 const globalDeaths = [];
@@ -36,59 +39,6 @@ const todayHundredConfirmed = [];
 const todayHundredDeaths = [];
 const todayHundredRecovered = [];
 
-const covidData = async () => {
-  const response = await fetch('https://corona-api.com/timeline');
-  const covidApiData = await response.json();
-
-  const { data } = covidApiData;
-
-  data.forEach((el) => {
-    dateStage.push(el.date);
-    globalConfirmed.push(el.confirmed);
-    globalDeaths.push(el.deaths);
-    globalRecovered.push(el.recovered);
-
-    globalHundredConfirmed.push(Math.round(el.confirmed / worldPopulationPer100));
-    globalHundredDeaths.push(Math.round(el.deaths / worldPopulationPer100));
-    globalHundredRecovered.push(Math.round(el.recovered / worldPopulationPer100));
-
-    todayHundredConfirmed.push(Math.round(el.new_confirmed / worldPopulationPer100));
-    todayHundredDeaths.push(Math.round(el.new_deaths / worldPopulationPer100));
-    todayHundredRecovered.push(Math.round(el.new_recovered / worldPopulationPer100));
-  });
-
-  newConfirmed.push(data[0].new_confirmed);
-  newRecovered.push(data[0].new_recovered);
-  newDeaths.push(data[0].new_deaths);
-  lastDate += data[0].date;
-
-  sortData();
-  createDailyChart();
-  createHundredChart();
-};
-
-const sortData = () => {
-  globalConfirmed.sort((a, b) => a - b);
-  globalDeaths.sort((a, b) => a - b);
-  globalRecovered.sort((a, b) => a - b);
-
-  globalHundredConfirmed.sort((a, b) => a - b);
-  globalHundredDeaths.sort((a, b) => a - b);
-  globalHundredRecovered.sort((a, b) => a - b);
-
-  todayHundredConfirmed.sort((a, b) => a - b);
-  todayHundredDeaths.sort((a, b) => a - b);
-  todayHundredRecovered.sort((a, b) => a - b);
-
-  dateStage.sort();
-
-  dateStage.forEach((el) => {
-    rightDate.push(formattedDate(el));
-  });
-
-//   lastDate += dateStage[dateStage.length - 1];
-};
-
 const monthsNames = [
   'Jan',
   'Feb',
@@ -109,61 +59,23 @@ const formattedDate = (parseDate) => {
   return `${date.getDate()} ${monthsNames[date.getMonth()]}`;
 };
 
-const createGlobalChart = async () => {
-  worldCases.classList.add('active');
+const sortData = () => {
+  globalConfirmed.sort((a, b) => a - b);
+  globalDeaths.sort((a, b) => a - b);
+  globalRecovered.sort((a, b) => a - b);
 
-  await covidData();
+  globalHundredConfirmed.sort((a, b) => a - b);
+  globalHundredDeaths.sort((a, b) => a - b);
+  globalHundredRecovered.sort((a, b) => a - b);
 
-  globalChartCreated = new Chart(chartGlobal, {
-    type: 'line',
-    data: {
-      datasets: [
-        {
-          label: 'Global Confirmed',
-          data: globalConfirmed,
-          fill: false,
-          borderColor: '#3e95cd',
-          backgroundColor: '#3e95cd',
-          borderWidth: 1,
-        },
-        {
-          label: 'Global Recovered',
-          data: globalRecovered,
-          fill: false,
-          borderColor: '#8e5ea2',
-          backgroundColor: '#8e5ea2',
-          borderWidth: 1,
-        },
-        {
-          label: 'Global Deaths',
-          data: globalDeaths,
-          fill: false,
-          borderColor: '#3cba9f',
-          backgroundColor: '#3cba9f',
-          borderWidth: 1,
-        },
-      ],
-      labels: rightDate,
-    },
-    options: {
-      legend: {
-        display: true,
-        position: 'top',
-        labels: {
-          boxWidth: 50,
-          fontColor: 'black',
-        },
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-          },
-        }],
-      },
-      responsive: true,
-      maintainAspectRatio: false,
-    },
+  todayHundredConfirmed.sort((a, b) => a - b);
+  todayHundredDeaths.sort((a, b) => a - b);
+  todayHundredRecovered.sort((a, b) => a - b);
+
+  dateStage.sort();
+
+  dateStage.forEach((el) => {
+    rightDate.push(formattedDate(el));
   });
 };
 
@@ -213,6 +125,8 @@ const createDailyChart = () => {
       maintainAspectRatio: false,
     },
   });
+
+  return dailyChartCreated;
 };
 
 const hundredChartAction = () => {
@@ -287,16 +201,125 @@ const createHundredChart = () => {
         labels: {
           boxWidth: 20,
           fontSize: 11,
-          fontColor: 'antiquewhite',
+          fontColor: 'darkslategray',
         },
       },
       responsive: true,
       maintainAspectRatio: false,
     },
   });
+
+  return hundredChartCreated;
+};
+
+const covidData = async () => {
+  const response = await fetch('https://corona-api.com/timeline');
+  const covidApiData = await response.json();
+
+  const { data } = covidApiData;
+
+  data.forEach((el) => {
+    dateStage.push(el.date);
+    globalConfirmed.push(el.confirmed);
+    globalDeaths.push(el.deaths);
+    globalRecovered.push(el.recovered);
+
+    globalHundredConfirmed.push(Math.round(el.confirmed / worldPopulationPer100));
+    globalHundredDeaths.push(Math.round(el.deaths / worldPopulationPer100));
+    globalHundredRecovered.push(Math.round(el.recovered / worldPopulationPer100));
+
+    todayHundredConfirmed.push(Math.round(el.new_confirmed / worldPopulationPer100));
+    todayHundredDeaths.push(Math.round(el.new_deaths / worldPopulationPer100));
+    todayHundredRecovered.push(Math.round(el.new_recovered / worldPopulationPer100));
+  });
+
+  newConfirmed.push(data[0].new_confirmed);
+  newRecovered.push(data[0].new_recovered);
+  newDeaths.push(data[0].new_deaths);
+  lastDate += data[0].date;
+
+  sortData();
+  createDailyChart();
+  createHundredChart();
+};
+
+const chartButtonActive = (event) => {
+  const targetBtn = event.target;
+  const activeBtn = document.querySelector('.open');
+
+  if (targetBtn.getAttribute('class') === 'chart__control') {
+    return;
+  }
+
+  if (activeBtn !== null) {
+    activeBtn.classList.remove('open');
+  }
+  targetBtn.classList.add('open');
+};
+
+const createGlobalChart = async () => {
+  worldCases.classList.add('active');
+
+  await covidData();
+
+  globalChartCreated = new Chart(chartGlobal, {
+    type: 'line',
+    data: {
+      datasets: [
+        {
+          label: 'Global Confirmed',
+          data: globalConfirmed,
+          fill: false,
+          borderColor: '#3e95cd',
+          backgroundColor: '#3e95cd',
+          borderWidth: 1,
+        },
+        {
+          label: 'Global Recovered',
+          data: globalRecovered,
+          fill: false,
+          borderColor: '#8e5ea2',
+          backgroundColor: '#8e5ea2',
+          borderWidth: 1,
+        },
+        {
+          label: 'Global Deaths',
+          data: globalDeaths,
+          fill: false,
+          borderColor: '#3cba9f',
+          backgroundColor: '#3cba9f',
+          borderWidth: 1,
+        },
+      ],
+      labels: rightDate,
+    },
+    options: {
+      legend: {
+        display: true,
+        position: 'top',
+        labels: {
+          boxWidth: 50,
+          fontColor: 'black',
+        },
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+
+  return globalChartCreated;
 };
 
 globalCasesButton.addEventListener('click', () => globalChartAction());
 dailyCasesButton.addEventListener('click', () => dailyChartAction());
 hundredCasesButton.addEventListener('click', () => hundredChartAction());
+chartBtnContainer.addEventListener('click', (event) => chartButtonActive(event));
+
 createGlobalChart();
